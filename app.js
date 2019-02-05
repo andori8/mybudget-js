67cf9115e621dece8d1ed2d8d92cff4a132e4304
andori8 <falcsmike7@gmail.com>
2019-02-05 15:23:07 -0500

    testing data structure

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 const budgetController = (function() {
 
+  const Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  const Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  let data = {
+    allItems: { exp: [], inc: [] },
+    totals: { exp: 0, inc: 0 }
+  }
+
+  return {
+    addItem: function(type, desc, val) {
+      let newItem, ID;
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      if (type === "exp") {
+        newItem = new Expense(ID, desc, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, desc, val);
+      }
+
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data)
+    }
+  }
+
 })()
 
 const UIController = (function() {
@@ -27,18 +68,29 @@ const UIController = (function() {
 
 const controller = (function(budgetCtrl, UICtrl) {
 
-  let DOM = UICtrl.getDOMstrings()
+  let setupEventListeners = function() {
+    let DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-  let ctrlAddItem = function() {
-    let input = UICtrl.getInput()
-    console.log(input);
+    document.addEventListener("keypress", function(e) {
+      if (e.keyCode === 13 || e.which === 13) {
+        ctrlAddItem();
+      }
+    })
   }
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+  let ctrlAddItem = function() {
+    let input = UICtrl.getInput();
+    let newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+  }
 
-  document.addEventListener("keypress", function(e) {
-    if (e.keyCode === 13 || e.which === 13) {
-      ctrlAddItem()
+  return {
+    init: function() {
+      console.log("Application has started.");
+      setupEventListeners();
     }
-  })
+  }
+
 })(budgetController, UIController);
+
+controller.init()
